9ba9c56f225eeff75679b7c396c70a57b279b3ce
fahad <mdfahadmulla@gmail.com>
2020-07-04 13:33:05 +0530

    javascript file

--- script.js
+++ script.js
@@ -2,16 +2,22 @@ function validateForm() {
     var nameValue = document.forms["myform"]["name"].value;
     var emailValue = document.forms["myform"]["email"].value;
     var passwordValue = document.forms["myform"]["password"].value;
+    var alphabets = /^[A-Za-z]+$/;
+    var email
 
     if (nameValue == "") {
         alert("Name Field cannot be empty");
+        return false;
+
+    } else if (!nameValue.match(alphabets)) {
+        alert("Name cannot be numbers or symbols");
         return false;
     }
 
     if (emailValue == "") {
         alert("Email Field cannot be empty");
         return false;
-    }
+    } else if(emailValue == )
 
     if (passwordValue == "") {
         alert("Password Field cannot be empty");
